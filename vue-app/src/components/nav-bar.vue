3c1d55463dee1db9fc24914a88678af22d5ce7ef
杨森 <sen.yang96@outlook.com>
2021-05-28 18:11:09 +0800

    Update about page

--- vue-app/src/components/nav-bar.vue
+++ vue-app/src/components/nav-bar.vue
@@ -1,9 +1,9 @@
 <template>
   <nav class="column is-2 menu">
-    <p class="menu-label">Menu</p>
+    <p class="menu-label">目录</p>
     <ul class="menu-list">
-      <router-link to="/products">Products</router-link>
-      <router-link to="/about">About</router-link>
+      <router-link to="/products">转换工具</router-link>
+      <router-link to="/about">帮助</router-link>
     </ul>
   </nav>
 </template>
--- vue-app/src/views/about.vue
+++ vue-app/src/views/about.vue
@@ -1,17 +1,39 @@
 <template>
   <div class="content-container">
     <div class="content-title-group not-found">
-      <h2 class="title">Product Wish List</h2>
+      <h2 class="title">如何使用</h2>
       <p>
-        This project was created to help represent a fundamental app written
-        with Vue. The shopping theme is used throughout the app.
+        将中国知网导出的 NoteExpress 文献记录转换成 BibTeX 文献记录。
       </p>
+      <br />
+      <p>
+        Bib 是写论文时十分通用的参考文献读取格式，如 Google Scholar，dblp等都支持Bib的导出。
+        然而国内的知网并不支持，因此我们在使用bib管理参考文献(mendeley 或 Latex)时，并不能很好地导入知网上的参考文献。
+        因此，将CNKI文献格式转换成BIB具有重大的研究意义和应用价值。
+      </p>
+      <br />
+
+      <h3><b>在知网上将 NoteExpress 参考文献内容(.net)复制到剪贴板</b></h3>
+      <img src="../assets/getNoteExpress.png"/>
+      <br />
+      <p>
+        粘贴NoteExpress内容到“转换工具->上方文本框”，点击“转换成Bib”。
+        <br />
+        根据你的需求 “复制到粘贴板” 或 “保存为文件”。
+      </p>
+      <br />
+
       <br />
       <h2 class="title">Resources</h2>
       <ul>
         <li>
-          <a href="https://github.com/MicrosoftDocs/mslearn-staticwebapp">
-            Code in GitHub
+          感谢<a href="https://github.com/Vopaaz/CNKI_2_BibTeX">
+            CNKI_2_BibTex on Github
+          </a>
+        </li>
+        <li>
+          我的网站代码<a href="https://github.com/YoungForest/CNKI2BIB_web">
+            CNKI2BIB_web on Github
           </a>
         </li>
       </ul>
