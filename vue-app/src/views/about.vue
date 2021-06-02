31dc23fc5137c556591d35e355dcca3a6b323cb8
杨森 <sen.yang96@outlook.com>
2021-06-02 17:44:49 +0800

    Add sponsor

--- vue-app/src/views/about.vue
+++ vue-app/src/views/about.vue
@@ -2,19 +2,19 @@
   <div class="content-container">
     <div class="content-title-group not-found">
       <h2 class="title">如何使用</h2>
-      <p>
-        将中国知网导出的 NoteExpress 文献记录转换成 BibTeX 文献记录。
-      </p>
+      <p>将中国知网导出的 NoteExpress 文献记录转换成 BibTeX 文献记录。</p>
       <br />
       <p>
-        Bib 是写论文时十分通用的参考文献读取格式，如 Google Scholar，dblp等都支持Bib的导出。
-        然而国内的知网并不支持，因此我们在使用bib管理参考文献(mendeley 或 Latex)时，并不能很好地导入知网上的参考文献。
+        Bib 是写论文时十分通用的参考文献读取格式，如 Google
+        Scholar，dblp等都支持Bib的导出。
+        然而国内的知网并不支持，因此我们在使用bib管理参考文献(mendeley 或
+        Latex)时，并不能很好地导入知网上的参考文献。
         因此，将CNKI文献格式转换成BIB具有重大的研究意义和应用价值。
       </p>
       <br />
 
       <h3><b>在知网上将 NoteExpress 参考文献内容(.net)复制到剪贴板</b></h3>
-      <img src="../assets/getNoteExpress.png"/>
+      <img src="../assets/getNoteExpress.png" />
       <br />
       <p>
         粘贴NoteExpress内容到“转换工具->上方文本框”，点击“转换成Bib”。
@@ -37,11 +37,23 @@
           </a>
         </li>
         <li>
-          我的网站后端代码<a href="https://github.com/YoungForest/CNKI2BIB_Backend">
+          我的网站后端代码<a
+            href="https://github.com/YoungForest/CNKI2BIB_Backend"
+          >
             CNKI2BIB_Backend on Github
           </a>
         </li>
       </ul>
+      <br />
+
+      <br />
+      <h2 class="title">赞赏</h2>
+      <p>
+        虽然本项目完全免费使用，但作者编写和维护此网站花费了诸多心血。因此，考虑<b>给作者赏只鸡腿</b>吧。
+      </p>
+      <br />
+      <img src="../assets/alipay.png" width="40%" style="margin: 10px" />
+      <img src="../assets/wechatpay.jpeg" width="40%" style="margin: 10px" />
     </div>
   </div>
 </template>
